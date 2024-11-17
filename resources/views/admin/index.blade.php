ce654613aca84e866b8710fdd98463d6af6261ec
Andrés López Corrales <andreslopezcorrales326@gmail.com>
2024-11-17 13:25:10 -0700

    required a imagenes

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -35,7 +35,7 @@
                 <input type="text" name="LugarEvento" id="LugarEvento" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
                 <br>
                 <label for="imagen_path" class="block mb-2 text-sm font-medium text-gray-900 dark:text-blue-700">Imagen del Evento:</label>
-                <input type="file" name="imagen_path" id="imagen_path" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" accept=".jpg, .jpeg, .png">
+                <input type="file" name="imagen_path" id="imagen_path" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" accept=".jpg, .jpeg, .png" required>
                 <br>
                 <button type="submit" class="py-2.5 px-5 me-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-full border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Agregar Evento</button>
             </form>
